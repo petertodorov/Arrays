e42778f1c82c38b33877225863fa642ab7ad35d3
Peter Todorov <petodorov@gmail.com>
2016-11-09 14:00:50 +0200

    ArrayLab2

---
+++ ArraysLabAndExercises/1ArraysLab/2ReverseAnArrayOFfIntegers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class ArraysLab
+{
+
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int numberOfLines = n;
+        string[] arrayOfNumbers = new string[n];
+        for (int i = 0; i < arrayOfNumbers.Length; i++)
+        {
+            arrayOfNumbers[i] = Console.ReadLine();
+        }
+        Array.Reverse(arrayOfNumbers);
+
+        foreach (var number in arrayOfNumbers)
+        {
+            Console.Write($"{number} ");
+        }
+
+        
+    }
+}
