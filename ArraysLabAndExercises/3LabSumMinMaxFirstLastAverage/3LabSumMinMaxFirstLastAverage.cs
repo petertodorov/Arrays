9c9741708a76ee84591eb1fa8a231722eecd6d40
Peter Todorov <petodorov@gmail.com>
2016-11-09 14:34:51 +0200

    Lab2

---
+++ ArraysLabAndExercises/3LabSumMinMaxFirstLastAverage/3LabSumMinMaxFirstLastAverage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class LabSumMinMaxFirstLastAverage
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] numbers = new int[n];
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = int.Parse(Console.ReadLine());
+        }
+        int sum = numbers.Sum();
+        int min = numbers.Min();
+        int max = numbers.Max();
+        int first = numbers.First();
+        int last = numbers.Last();
+        double average = numbers.Average();
+        
+        Console.WriteLine($"\n{sum}\n{min}\n{max}\n{first}\n{last}\n{average}");
+
+
+    }
+}
+
