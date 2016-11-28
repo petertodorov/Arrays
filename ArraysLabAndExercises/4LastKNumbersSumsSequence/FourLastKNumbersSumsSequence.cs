3b3ba6683f46b4d5cccd4c249090f6b9410b944c
Peter Todorov <petodorov@gmail.com>
2016-11-28 14:18:22 +0200

    Solved

---
+++ ArraysLabAndExercises/4LastKNumbersSumsSequence/FourLastKNumbersSumsSequence.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class FourLastKNumbersSumsSequence
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int k = int.Parse(Console.ReadLine());
+        long [] sequence = new long [n];
+        sequence[0] = 1;
+
+        for (int i = 1; i < n; i++)
+        {
+            int start = i - k;
+            int end = i - 1;
+            sequence[i] = SumElements(sequence, start, end);
+        }
+        PrintArray(sequence);
+    }
+
+    private static long SumElements(long [] sequence, int start, int end)
+    {
+        long sum = 0;
+        for (int i = start; i <= end; i++) 
+        {
+            if (i>=0)
+            {
+                sum += sequence[i];
+            }
+        }
+       return sum;
+    }
+    private static void PrintArray(long[] sequence)
+    {
+        Console.WriteLine(String.Join(" ", sequence));
+    }
+}
+
+
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            int[] numbers = new int[n];
+            numbers[0] = 1;
+            SumLastKNumbers(numbers,k);
+        }
+
+        private static void SumLastKNumbers(int[] numbers, int k)
+        {
+            for (int currentNumber = 1; currentNumber < numbers.Length; currentNumber++)
+            {
+                int start =  currentNumber- k;
+                int end = currentNumber - 1;
+                int sum = 0;
+                for (int i = start; i <end; i++)
+                {
+                    sum += numbers[i];
+                }
+                numbers[currentNumber] = sum;
+               
+            }
+            PrintArray(numbers);
+        }
+
+        private static void PrintArray(int[] numbers)
+        {
+            Console.WriteLine(String.Join(" ", numbers));
+        }
+    }
+}
