0405e12e329df52e469df16c70ac8748af62b5e6
Peter Todorov <petodorov@gmail.com>
2016-11-30 14:29:40 +0200

    added method without using  dot.something

--- ArraysLabAndExercises/6RoundingNumbersAwayFromZero/SixRoundingNumbersAwayFromZero.cs
+++ ArraysLabAndExercises/6RoundingNumbersAwayFromZero/SixRoundingNumbersAwayFromZero.cs
@@ -13,8 +13,32 @@ using System.Threading.Tasks;
         {
             int roundedNum = (int)Math.Round(numbers[i], MidpointRounding.AwayFromZero);
             Console.WriteLine($"{numbers[i]} => {roundedNum}");
+            
         }
+        Console.WriteLine("\nResults from SimpleSolution\n");
+        SimpleSoulion(numbers);
     }
 
+    private static void SimpleSoulion(decimal[] numbers)
+    {
+        foreach (var num in numbers)
+        {
+            if (num>0)
+            {
+                int temp = (int)(num + 0.5M);
+                Console.WriteLine($"{num} => {temp}");
+            }
+            else if (num<0)
+            {
+                int temp = (int) num;
+                temp = (int)(num - 0.5M);
+                Console.WriteLine($"{num} => {temp}");
+            }
+            else
+            {
+                Console.WriteLine("0 => 0");
+            }
+        }
+    }
 }
 
