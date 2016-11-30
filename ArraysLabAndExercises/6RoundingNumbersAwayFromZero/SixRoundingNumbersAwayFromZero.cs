dc4082dc1e5703b44fd679fc363de473f2e2482c
Peter Todorov <petodorov@gmail.com>
2016-11-30 12:53:53 +0200

    Solved

---
+++ ArraysLabAndExercises/6RoundingNumbersAwayFromZero/SixRoundingNumbersAwayFromZero.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    class SixRoundingNumbersAwayFromZero
+    {
+    static void Main(string[] args)
+    {
+        decimal[] numbers = Console.ReadLine().Replace('.', ',').Split(' ').Select(decimal.Parse).ToArray();
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            decimal roundedNum = Math.Round(numbers[i], MidpointRounding.AwayFromZero);
+            Console.WriteLine($"{numbers[i]} => {roundedNum}");
+        }
+    }
+
+}
+
