c71235bce3ed3e67160cfcf8d4733b343b865206
Peter Todorov <petodorov@gmail.com>
2016-11-30 15:37:06 +0200

    Solved

---
+++ ArraysLabAndExercises/7ReverseAnArrayOfStrings/SevenReverseAnArrayOfStrings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class SevenReverseAnArrayOfStrings
+{
+    static void Main(string[] args)
+    {
+        string [] input= Console.ReadLine().Split(' ').Select(Convert.ToString).ToArray();
+        Array.Reverse(input);
+        Console.Write(String.Join ("  ",input));
+    }
+}
