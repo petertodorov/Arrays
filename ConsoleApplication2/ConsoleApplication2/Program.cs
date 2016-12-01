30a07b7ff454ca3e40c1b05303830a4d724ffe36
Peter Todorov <petodorov@gmail.com>
2016-12-01 16:52:24 +0200

    s

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class FiveTripleSumAnotherSolution
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        for (int i = 0; i < numbers.Length/2; i++)
+        {
+            SwapElements(numbers, i, numbers.Length - 1 - i);
+            
+
+        }
+        
+        Console.WriteLine(String.Join(" ", numbers));
+    }
+
+    private static void SwapElements(int[] numbers, int i, int j)
+    {
+       
+        int old = numbers[i];
+        numbers[i] = numbers[j];
+        numbers[j] = old;
+    }
+} 
