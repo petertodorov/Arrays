80772fc6700dec097ad5b32c7973f4449c4fbc92
Peter Todorov <petodorov@gmail.com>
2016-12-03 16:06:49 +0200

    Solved

---
+++ ArraysLabAndExercises/9CondenseArrayToNumber/CondenseArrayToNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class CondenseArrayToNumber
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        
+        while (numbers.Length>1)
+        {
+            int[] condensed = new int[numbers.Length - 1];
+            for (int i = 0; i < condensed.Length; i++)
+            {
+                condensed[i] = numbers[i] + numbers[i + 1];
+            }
+            numbers = condensed;
+        }
+        
+        Console.WriteLine(String.Join(" ", numbers));
+        
+    }
+}
+
