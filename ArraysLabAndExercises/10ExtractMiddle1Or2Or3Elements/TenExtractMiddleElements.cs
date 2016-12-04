9a41931c2497fac45b2026fbfe85786595f1b6b9
Peter Todorov <petodorov@gmail.com>
2016-12-04 11:52:41 +0200

    .

--- ArraysLabAndExercises/10ExtractMiddle1Or2Or3Elements/TenExtractMiddleElements.cs
+++ ArraysLabAndExercises/10ExtractMiddle1Or2Or3Elements/TenExtractMiddleElements.cs
@@ -12,19 +12,18 @@ class TenExtractMiddleElements
         int n = numbers.Length;
         int coef = numbers.Length / 2;
         int coefOdd = ((numbers.Length - 1) / 2);
-        if (n==1)
+        if (n == 1)
         {
             Console.WriteLine(numbers[0]);
         }
-        else if (n%2==0)
+        else if (n % 2 == 0)
         {
-            Console.WriteLine($"{numbers[coef- 1]},  {numbers[coef]}");
+            Console.WriteLine($"{numbers[coef - 1]},  {numbers[coef]}");
         }
         else if (n % 2 != 0)
         {
-            Console.WriteLine($"{numbers[coefOdd-1]}, {numbers[coefOdd]}, {numbers[coefOdd+1]}");
+            Console.WriteLine($"{numbers[coefOdd - 1]}, {numbers[coefOdd]}, {numbers[coefOdd + 1]}");
         }
-
     }
 }
 
