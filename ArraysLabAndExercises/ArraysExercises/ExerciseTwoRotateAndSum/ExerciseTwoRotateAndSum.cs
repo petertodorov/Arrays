6e95a2ab5fd8e01a464bf29872834e05dd7f4d74
Peter Todorov <petodorov@gmail.com>
2016-12-06 14:10:10 +0200

    .

--- ArraysLabAndExercises/ArraysExercises/ExerciseTwoRotateAndSum/ExerciseTwoRotateAndSum.cs
+++ ArraysLabAndExercises/ArraysExercises/ExerciseTwoRotateAndSum/ExerciseTwoRotateAndSum.cs
@@ -3,21 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-<<<<<<< HEAD
-    class ExerciseTwoRotateAndSum
-{
-        static void Main(string[] args)
-        {
-
-=======
-
 
     class ExerciseTwoRotateAndSum
     {
         static void Main(string[] args)
         {
             
->>>>>>> origin/master
+
             int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
             int rotation = int.Parse(Console.ReadLine());
             int[] result = new int[numbers.Length];
