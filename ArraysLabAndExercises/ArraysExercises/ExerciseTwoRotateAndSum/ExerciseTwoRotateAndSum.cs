7ccb234ef767b71be441e64a57763baf441fa72c
Peter Todorov <petodorov@gmail.com>
2016-12-06 14:08:07 +0200

    .

--- ArraysLabAndExercises/ArraysExercises/ExerciseTwoRotateAndSum/ExerciseTwoRotateAndSum.cs
+++ ArraysLabAndExercises/ArraysExercises/ExerciseTwoRotateAndSum/ExerciseTwoRotateAndSum.cs
@@ -3,13 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace ExerciseTwoRotateAndSum
+    class ExerciseTwoRotateAndSum
 {
-    class Program
-    {
         static void Main(string[] args)
         {
+
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int rotation = int.Parse(Console.ReadLine());
+            int[] result = new int[numbers.Length];
+            RotateAndPrint(numbers, result, rotation);
+        }
+        private static void RotateAndPrint(int[] numbers, int[] result, int rotation)
+        {
+            for (int i = 0; i < rotation; i++)
+            {
+                RotateArr(numbers);
+                SumResult(result, numbers);
+            }
+            Console.WriteLine(String.Join(" ", result));
+        }
+        private static void SumResult(int[] result, int[] numbers)
+        {
+            for (int j = 0; j < numbers.Length; j++)
+            {
+                result[j] += numbers[j];
+            }
+        }
+        private static void RotateArr(int[] numbers)
+        {
+            int lastElement = numbers[numbers.Length - 1];
+            Array.Copy(numbers, 0, numbers, 1, numbers.Length - 1);
+            numbers[0] = lastElement;
         }
     }
-}
+
